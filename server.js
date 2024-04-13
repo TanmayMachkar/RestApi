b14e666d9c83ebbfddbe8e710940936804c2fb8c
TanmayMachkar <tanmaymacho@gmail.com>
2024-04-13 19:47:45 +0530

    all basic postman methods done

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const studentRoutes = require('./src/student/routes');
 const app = express();
 const port = 3000;
 
+app.use(express.json()); //allows to post and use json
+
 app.get("/", (req, res) => {
 	res.send("Hello machkar");
 })
@@ -13,3 +15,7 @@ app.use("/api/v1/students", studentRoutes);
 app.listen(port, () => 
 	console.log(`App listening on port ${port}`)
 );
+
+
+//get id: in params
+//post info: in body in form of json
--- src/student/routes.js
+++ src/student/routes.js
@@ -4,5 +4,9 @@ const controller = require('./controller');
 const router = Router();
 
 router.get("/", controller.getStudents );
+router.post('/', controller.addStudents);
+router.get('/:id', controller.getStudentById);
+router.put('/:id', controller.updateStudent);
+router.delete('/:id', controller.removeStudent);
 
 module.exports = router;
